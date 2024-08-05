36b303630c7fe39ae3513f3ddad45b42ec390740
HakaHakiev <122813219+hhakiev22@users.noreply.github.com>
2024-08-06 00:03:53 +0300

    Create navigation bar

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Food-Delivery
 
 ## 1.Create frontend folder
+
+## 2.Create Navigation bar
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,13 @@
 // import React from "react";
 
+import Navbar from "./components/navbar/Navbar";
+
 const App = () => {
-  return <div></div>;
+  return (
+    <div className="app">
+      <Navbar />
+    </div>
+  );
 };
 
 export default App;
