7645ee69f99c5d13333dce8c53d1134528a00dcb
HakaHakiev <122813219+hhakiev22@users.noreply.github.com>
2024-08-11 15:32:25 +0300

    Create menu items

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -1,11 +1,16 @@
 // import React from "react";
+import { useState } from "react";
+import ExploreMenu from "../../components/exploreMenu/ExploreMenu";
 import Header from "../../components/header/Header";
 import "./Home.css";
 
 const Home = () => {
+  const [category, setCategory] = useState("All");
+
   return (
     <div>
       <Header />
+      <ExploreMenu category={category} setCategory={setCategory} />
     </div>
   );
 };
